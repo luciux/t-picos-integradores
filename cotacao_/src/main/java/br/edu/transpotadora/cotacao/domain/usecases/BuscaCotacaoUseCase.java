5935f01e392450d78a0410f2ee3a8d3ae50a28dd
Luciano Jr <l_u_cio@hotmail.com>
2022-09-23 00:05:02 -0300

    Removido projeto Gradle deixado apenas maven

--- cotacao_/src/main/java/br/edu/transpotadora/cotacao/domain/usecases/BuscaCotacaoUseCase.java
+++
@@ -1,9 +0,0 @@
-package br.edu.transpotadora.cotacao.domain.usecases;
-
-import br.edu.transpotadora.cotacao.domain.entities.CotacaoEntity;
-
-public interface BuscaCotacaoUseCase {
-
-    public Boolean execute(CotacaoEntity cotacao);
-
-}
